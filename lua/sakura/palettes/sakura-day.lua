abe46bee1794a24507fc7b3dc4f18ded2b6f2a1b
Connor Wyatt <connorwyatt1@gmail.com>
2025-02-25 13:29:01 +0000

    feat: improve light theme colors

--- lua/sakura/palettes/sakura-day.lua
+++ lua/sakura/palettes/sakura-day.lua
@@ -6,13 +6,13 @@ local text = { 230, 15, 40, }
 
 local white = "#ffffff"
 
-local pink = c.desaturate(c.darken(color_palette.pink, 10), 15)
-local pink_background = c.desaturate(c.lighten(pink, 30), 5)
-local yellow = c.desaturate(c.darken(color_palette.yellow, 5), 5)
-local green = c.desaturate(c.darken(color_palette.green, 10), 10)
-local cyan = c.desaturate(c.darken(color_palette.cyan, 5), 10)
-local purple = c.desaturate(c.darken(color_palette.purple, 10), 15)
-local red = c.desaturate(c.darken(color_palette.red, 5), 15)
+local pink = c.darken(color_palette.pink, 5)
+local pink_background = c.desaturate(c.lighten(pink, 27), 15)
+local yellow = c.saturate(color_palette.yellow, 10)
+local green = c.darken(color_palette.green, 10)
+local cyan = color_palette.cyan
+local purple = c.darken(color_palette.purple, 5)
+local red = color_palette.red
 
 return {
     is_dark = false,
@@ -26,15 +26,15 @@ return {
     pink = pink,
     pink_background = pink_background,
     yellow = yellow,
-    yellow_background = c.desaturate(c.lighten(yellow, 35), 15),
+    yellow_background = c.lighten(yellow, 30),
     green = green,
-    green_background = c.desaturate(c.lighten(green, 35), 10),
+    green_background = c.lighten(green, 35),
     cyan = cyan,
-    cyan_background = c.desaturate(c.lighten(cyan, 25), 15),
+    cyan_background = c.lighten(cyan, 27),
     purple = purple,
-    purple_background = c.desaturate(c.lighten(purple, 30), 5),
+    purple_background = c.lighten(purple, 25),
     red = red,
-    red_background = c.desaturate(c.lighten(red, 30), 15),
+    red_background = c.lighten(red, 25),
     highlight_low = pink_background,
     highlight_medium = c.darken(pink_background, 3),
     highlight_high = c.darken(pink_background, 6),
